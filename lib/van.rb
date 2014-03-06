6c3bece1d279e279edcdc261545ebd7a29400dfd
Mikhail Dubov <mdubov@gmail.com>
2014-03-06 12:29:29 +0000

    Improved van functionality

--- lib/van.rb
+++ lib/van.rb
@@ -8,9 +8,23 @@ class Van
     self.capacity = options.fetch(:capacity, capacity)
   end
 
-  def collect(station)
-    station.broken_bikes.each {|bike| bikes << bike}
+  def collect_broken(station)
+    station.broken_bikes.each {|bike| dock(bike)}
     station.release_broken
+  end
+
+  def release_bikes_to(container)
+    bikes.each do |bike|
+      container.dock(bike)
+      release(bike)
+    end
+  end
+
+  def collect_fixed(garage)
+    garage.bikes.each do |bike| 
+      self.dock(bike)
+      garage.release(bike)
+    end 
   end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -5,13 +5,39 @@ describe Van do
   let(:van) {Van.new}
   
   let(:station) {DockingStation.new}
+  let(:garage) {Garage.new}
+
   it 'should collect broken bikes from station' do 
     broken_bike = Bike.new
     broken_bike.break
     station.dock(broken_bike)
     expect(station.broken_bikes).to eq [broken_bike]
-    van.collect(station)
+    van.collect_broken(station)
     expect(station.broken_bikes).to be_empty
     expect(van.bikes).to eq [broken_bike]
+  end
+
+  it 'should release bikes' do 
+    bike = Bike.new
+    van.dock(bike)
+    van.release_bikes_to(station)
+    expect(van.bikes).to be_empty
+    expect(station.bikes).to eq [bike]
+  end
+
+  it 'shoudld collect fixed bikes from garage' do 
+    bike = Bike.new
+    garage.dock(bike)
+    van.collect_fixed(garage)
+    expect(van.bikes).to eq [bike]
+    expect(garage.bikes).to be_empty
+  end
+
+  it 'should not accept new bikes when full' do 
+    20.times {van.dock(Bike.new)}
+    broken_bike = Bike.new
+    broken_bike.break
+    station.dock(broken_bike)
+    expect(lambda { van.collect_broken(station) }).to raise_error(RuntimeError)
   end
 end
